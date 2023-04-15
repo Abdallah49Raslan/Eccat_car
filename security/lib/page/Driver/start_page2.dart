0e4aaaf64f76b0f53fc8414c6f0c7d9685bb2209
Abdallah49Raslan <abdallahraslan682@gmail.com>
2023-04-15 18:16:52 +0200

    added ibrahim file

--- security/lib/page/Driver/start_page2.dart
+++ security/lib/page/Driver/start_page2.dart
@@ -2,32 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+import '../../Iot/mainpage.dart';
 import '../Health Care/darkmode.dart';
 import '../login_page.dart';
 
 class StartPage2 extends StatelessWidget {
   const StartPage2({Key? key}) : super(key: key);
 
-  void _showMenu(BuildContext context) {
-    // Callback function to show the menu options
-    // Implement the desired menu functionality here
-    print('Menu button pressed!');
+  void _showHealthCare(BuildContext context) {
+    // Callback function to navigate to Health Care page
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const darkmode()),
+    );
+  }
+
+  void _showIoT(BuildContext context) {
+    // Callback function to navigate to IoT page
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => const Home()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Start Page 2'),
         actions: [
-          IconButton(
-            // Use Icons.menu for three dashes menu icon
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              _showMenu(context); // Call the callback function on button press
-            },
+          PopupMenuButton(
+            // Use PopupMenuButton to show the menu options
+            itemBuilder: (BuildContext context) => <PopupMenuEntry>[
+              PopupMenuItem(
+                child: ListTile(
+                  title: const Text('Health Care'),
+                  onTap: () {
+                    Navigator.pop(context); // Close the menu
+                    _showHealthCare(
+                        context); // Call the callback function on button press
+                  },
+                ),
+              ),
+              PopupMenuItem(
+                child: ListTile(
+                  title: const Text('IoT'),
+                  onTap: () {
+                    Navigator.pop(context); // Close the menu
+                    _showIoT(
+                        context); // Call the callback function on button press
+                  },
+                ),
+              ),
+            ],
           ),
         ],
       ),
@@ -46,22 +74,6 @@ class StartPage2 extends StatelessWidget {
               },
               child: const Text(
                 'Log out',
-                style: TextStyle(fontSize: 18),
-              ),
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-              ),
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const darkmode()),
-                );
-              },
-              child: const Text(
-                'Health Care',
                 style: TextStyle(fontSize: 18),
               ),
               style: ElevatedButton.styleFrom(
