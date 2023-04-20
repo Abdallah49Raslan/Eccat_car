a0d834406d1e68ed222e08a60b15e73958d5e165
Abdallah49Raslan <abdallahraslan682@gmail.com>
2023-04-20 15:33:10 +0200

    eidt login page

--- security/lib/main.dart
+++ security/lib/main.dart
@@ -1,39 +1,22 @@
 import 'package:security/page/Authintication/login_page.dart';
-import 'package:security/page/Customer/Custom_start.dart';
-import 'package:security/page/Driver/Driver_start.dart';
+import 'package:security/page/Health%20Care/darkmode.dart';
+import 'package:security/page/User_Profile.dart';
+import 'package:security/page/spalsh_Page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:security/page/Owner/Owner_start.dart';
-import 'package:security/page/spalsh_Page.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  String? role = prefs.getString('role');
-  Widget startPage;
-  if (role == null) {
-    startPage = LoginPage();
-  } else if (role == 'owner') {
-    startPage = OwnerStartPage();
-  } else if (role == 'driver') {
-    startPage = DriverStartPage();
-  } else if (role == 'customer') {
-    startPage = CustomerStartPage();
-  } else {
-    startPage = LoginPage();
-  }
-  runApp(MyApp(startPage));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final Widget startPage;
-
-  const MyApp(this.startPage);
+  const MyApp({Key? key}) : super(key: key);
 
   ThemeData themeData() {
     // Define your theme data here
@@ -46,7 +29,8 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: themeData(),
-      home: startPage,
+      home: const UserDetailPage(),
+      
     );
   }
 }
