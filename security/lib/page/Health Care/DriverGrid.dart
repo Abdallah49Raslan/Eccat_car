47fe389c5c7810cf707ef83ad9f2ac2d81bc56b2
nesreen abdallah <nesreen.abdallah04@gmail.com>
2023-05-04 01:24:51 +0300

    modified

--- security/lib/page/Health Care/DriverGrid.dart
+++ security/lib/page/Health Care/DriverGrid.dart
@@ -10,188 +10,191 @@ import 'package:security/page/Health Care/HealthConditions.dart';
 import 'package:security/page/Health Care/darkmode.dart';
 
 Widget drivergrid() {
-  return Container(
-    padding: EdgeInsets.only(right: 5, left: 1),
-    decoration: BoxDecoration(
-      color: Colors.white10,
-      border: Border.all(color: borderColor, width: 2),
-      borderRadius: BorderRadius.circular(6),
-    ),
-    child: Column(
-      children: [
-        Text(" Driver",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )),
-        SizedBox(
-          height: 20,
-        ),
-        Row(
-          children: [
-            //Icons Column
-            Column(
-              children: [
-                SvgPicture.asset("assets/icons/heart-rate.svg",
-                    height: 20, width: 20, color: Colors.red),
-                SizedBox(
-                  height: 5,
-                ),
-                SvgPicture.asset(
-                    "assets/icons/temperature-high-solid-svgrepo-com.svg",
-                    height: 25,
-                    width: 25,
-                    color: Colors.orange),
-                SizedBox(
-                  height: 5,
-                ),
-                Image.asset(
-                  "assets/icons/oxi3.png",
-                  height: 35,
-                  width: 35,
-                  color: Colors.blue,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Image.asset("assets/icons/alcohol.png",
-                    height: 18,
-                    width: 18,
-                    color: Color.fromARGB(255, 43, 168, 5)),
-              ],
-            ),
-            //Label Column
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Heart Rate",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
+  return Builder(builder: (context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.only(right: 5, left: 1),
+      decoration: BoxDecoration(
+        color: Colors.white10,
+        border: Border.all(color: borderColor, width: 2),
+        borderRadius: BorderRadius.circular(6),
+      ),
+      child: Column(
+        children: [
+          Text(" Driver",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            children: [
+              //Icons Column
+              Column(
+                children: [
+                  SvgPicture.asset("assets/icons/heart-rate.svg",
+                      height: 20, width: 20, color: Colors.red),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  SvgPicture.asset(
+                      "assets/icons/temperature-high-solid-svgrepo-com.svg",
+                      height: 25,
+                      width: 25,
+                      color: Colors.orange),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Image.asset(
+                    "assets/icons/oxi3.png",
+                    height: 35,
+                    width: 35,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Image.asset("assets/icons/alcohol.png",
+                      height: 18,
+                      width: 18,
+                      color: Color.fromARGB(255, 43, 168, 5)),
+                ],
+              ),
+              //Label Column
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Heart Rate",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Temp",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Oximeter",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    "Alcohol",
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              //Values Column
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    '$DdisplayheartRat',
+                    style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Temp",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '$Ddisplaytemp',
+                    style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Oximeter",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '$Ddisplayoximeter',
+                    style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Alcohol",
-                  textAlign: TextAlign.start,
-                  style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    '$displayalcohol',
+                    style: TextStyle(
                       fontSize: 20,
                       fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-              ],
-            ),
-            SizedBox(
-              width: 35,
-            ),
-            //Values Column
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  '$DdisplayheartRat',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                      color: Colors.white,
+                    ),
                   ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  '$Ddisplaytemp',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                ],
+              ),
+              Spacer(),
+              //Normal&Abnormal Column
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  DHeartRateCondition(),
+                  SizedBox(
+                    height: 5,
                   ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  '$Ddisplayoximeter',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  DTemperatureCondition(),
+                  SizedBox(
+                    height: 5,
                   ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  '$displayalcohol',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
+                  DOximeterCondition(),
+                  SizedBox(
+                    height: 5,
                   ),
-                ),
-              ],
-            ),
-            Spacer(),
-            //Normal&Abnormal Column
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                DHeartRateCondition(),
-                SizedBox(
-                  height: 5,
-                ),
-                DTemperatureCondition(),
-                SizedBox(
-                  height: 5,
-                ),
-                DOximeterCondition(),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Normal",
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-                    color: primaryColor,
+                  Text(
+                    "Normal",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
-        SizedBox(
-          width: 40,
-        ),
-      ],
-    ),
-  );
+                ],
+              ),
+            ],
+          ),
+          SizedBox(
+            width: 40,
+          ),
+        ],
+      ),
+    );
+  });
 }
